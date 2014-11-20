7d69d92fbb211fef1c689ed5f2f85b2a2cf71863
M V K Chaitanya <devchaitu18@gmail.com>
2014-11-20 18:23:24 +0530

    connect with dropbox
    connect button and js added.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ var APP_KEY = 'jcmzj1dcn8fg7qv';
 
 var client = new Dropbox.Client({key: APP_KEY});
 
+// Authenticate when the user clicks the connect button.
+$('#connectDropbox').click(function (e) {
+e.preventDefault();
+client.authenticate(updateAuthenticationStatus);
+});
+
 // Try to finish OAuth authorization.
 client.authenticate({interactive: false}, function (error) {
     if (error) {
