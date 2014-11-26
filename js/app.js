6fcb8b795d1a016022b6e958f32cfc0b80674d19
M V K Chaitanya <devchaitu18@gmail.com>
2014-11-26 16:10:40 +0530

    Refactored app.js

--- js/app.js
+++ js/app.js
@@ -1,56 +1,53 @@
 (function(){
 var app = angular.module('syncBudget',[]);
-var datastore = undefined;
-var isClientAuthenticated = false;
 
-app.controller('SyncDropBox', function(){
-	this.num = 100;
+	app.controller('SyncDropBox', function($scope){
+		
+		$scope.num = 100;
+		$scope.isClientAuthenticated = false;
+		console.log($scope.isClientAuthenticated);
+		
+		var APP_KEY = 'iiz72ijenjkeuw9';
+		var client = new Dropbox.Client({key: APP_KEY});
+		console.log(client);
 
-	this.isClientConnected = isClientAuthenticated;
-	console.log(this.isClientConnected);
-	var APP_KEY = 'iiz72ijenjkeuw9';
-	var client = new Dropbox.Client({key: APP_KEY});
-	console.log(client);
+		// Authenticate when the user clicks the connect button.
+		$('#connectDropbox').click(function (e) {
+			e.preventDefault();
+			client.authenticate();
+		});
 
-	// Authenticate when the user clicks the connect button.
-	$('#connectDropbox').click(function (e) {
-		e.preventDefault();
-		client.authenticate();
-	});
-
-	// Try to finish OAuth authorization.
-	client.authenticate({interactive: false}, function (error) {
-		if (error) {
-			alert('Authentication error: ' + error);
-		}
-	});
-
-	if (client.isAuthenticated()) {
-		isClientAuthenticated = true;
-		//console.log("client authenticated");
-		var datastoreManager = client.getDatastoreManager();
-		datastoreManager.openDefaultDatastore(function (error, userDatastore) {
+		// Try to finish OAuth authorization.
+		client.authenticate({interactive: false}, function (error) {
 			if (error) {
-				alert('Error opening default datastore: ' + error);
+				alert('Authentication error: ' + error);
 			}
-		datastore = userDatastore;
-	   
-	});
+		});
 
-	} 
+		if (client.isAuthenticated()) {
+			$scope.isClientAuthenticated = true;
+			//console.log("client authenticated");
+			var datastoreManager = client.getDatastoreManager();
+			datastoreManager.openDefaultDatastore(function (error, datastore) {
+				if (error) {
+					alert('Error opening default datastore: ' + error);
+				}
+			 $scope.userDatastore = datastore;
+		   
+		});
 
+		} 
 
-});
-
-app.controller('AddExpense', function(){
-		console.log(isClientAuthenticated);
-		if(isClientAuthenticated){
+		console.log($scope.isClientAuthenticated);
+		if($scope.isClientAuthenticated){
 		// Now you have a datastore. The next few examples can be included here.
 	    var testExpensesTable = datastore.getTable('testExpensesTable');
 	    console.log("testExpensesTable ::::")
 	    console.log(testExpensesTable);
 		}
 
-});
+
+	});
+
 
 })();
