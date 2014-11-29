8955f045d072e796296ac17da993b3a83653bae9
devchaitu18 <devchaitu18@gmail.com>
2014-11-29 21:36:44 +0530

    test

--- js/app.js
+++ js/app.js
@@ -78,7 +78,7 @@ var app = angular.module('syncBudget',['ngRoute','ui.bootstrap','ngTouch']);
 
 		app.controller('categoryIconCarousel', function($scope) {
 			console.log("Entered categoryIconCarousel");
-			$scope.myInterval = 8000;
+			$scope.myInterval = 80000;
 			var slides = $scope.slides = ["coffee",
 			"credit-card",
 			"cut",
